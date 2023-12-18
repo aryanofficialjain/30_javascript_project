b81572938aea100e56564a55e4665e5e6f18eed6
Aryan Jain <123539815+aryanofficialjain@users.noreply.github.com>
2023-12-18 23:13:18 +0530

    Add files via upload

---
+++ popup/script.js
@@ -0,0 +1,12 @@
+const click = document.querySelector("#click");
+const remove = document.querySelector("#remove");
+
+
+function show(){
+    document.querySelector(".pop").classList.add("active");
+}
+
+function removeit(){
+    document.querySelector(".pop").classList.remove("active");
+
+}
